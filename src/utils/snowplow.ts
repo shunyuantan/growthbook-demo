9ee5403dccb274cd35040fbf192fcf8be5ec4333
Shun Yuan <shun.tan@xendit.co>
2023-08-30 12:14:41 +0800

    chore(): add conditionals to testing tracker url

--- src/utils/snowplow.ts
+++ src/utils/snowplow.ts
@@ -1,13 +1,16 @@
-import {
-  newTracker,
-  setUserId,
-  TrackerConfiguration,
-} from '@snowplow/browser-tracker';
+import { newTracker, TrackerConfiguration } from '@snowplow/browser-tracker';
 
 const TESTING_TRACKER_URL =
   'https://c4ad0795-3fc7-4ebc-a0fa-fbb62c94fe6a.app.try-snowplow.com';
 
-const TRACKER_URL = 'https://snowplow-collector.iluma.ai';
+let TRACKER_URL: string;
+
+console.log('process.env.NODE', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  TRACKER_URL = 'https://snowplow-collector.iluma.ai';
+} else {
+  TRACKER_URL = TESTING_TRACKER_URL;
+}
 
 /**
  * @description SnowPlow tracker configuration
@@ -19,5 +22,6 @@ const configuration: TrackerConfiguration = {
 
 export const TRACKER_NAME = 'spNexGrowthBookDemo';
 export const initialiseSnowplow = () => {
-  return newTracker(TRACKER_NAME, TESTING_TRACKER_URL, configuration);
+  console.log('tracking_url ==> ', TRACKER_URL);
+  return newTracker(TRACKER_NAME, TRACKER_URL, configuration);
 };
